55c29cd1cd832ef9d58fc79367cc464c8e5ea14b
Filip <filnap1@gmail.com>
2021-06-14 19:31:00 +0200

    Added current garden user's e-mail address extraction from database. Minor typo fixes.

--- RODNaliczenia.py
+++ RODNaliczenia.py
@@ -2,14 +2,17 @@ import fdb
 import time
 import configparser
 
-print("Generator raportu opłat i nadpłat działkowców ROD. Wersja 2.6")
+print("Generator raportu opłat i nadpłat działkowców ROD. Wersja 2.7")
 print("Copyright (C) 2021 Filip Napierała i Marianna Humska")
-print("Data kompilacji 08.04.2021")
+print("Data kompilacji 14.06.2021r.")
 print("--------------------------------------------------------------------------------------------------------")
-print("Niniejszy program jest wolnym oprogramowaniem - możesz go rozpowszechniać dalej i/lub modyfikować na warunkach Powszechnej Licencji Publicznej GNU")
+print("Niniejszy program jest wolnym oprogramowaniem - możesz go rozpowszechniać dalej i/lub modyfikować ")
+print("na warunkach Powszechnej Licencji Publicznej GNU")
 print("wydanej przez Fundację Wolnego Oprogramowania, według wersji 3 tej Licencji lub dowolnej z późniejszych wersji.")
-print("Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny - jednak BEZ ŻADNEJ GWARANCJI, nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ,")
-print("albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. Bliższe informacje na ten temat można uzyskać z Powszechnej Licencji Publicznej GNU.")
+print("Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny - jednak BEZ ŻADNEJ GWARANCJI,")
+print("nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ,")
+print("albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. Bliższe informacje na ten temat można uzyskać z")
+print("Powszechnej Licencji Publicznej GNU.")
 print("Powszechnej Licencji Publicznej GNU powinna zostać ci dostarczona razem z tym programem")
 print("--------------------------------------------------------------------------------------------------------")
 print("Program powinien być uruchamiany na serwerze")
@@ -35,13 +38,13 @@ print("Wszystkie dane wpisane prawidłowo")
 cur.execute("SELECT NUMERDZIALKI FROM \"@PZD_DZIALKI\" ")
 listadzialek = cur.fetchall()
 # Creating list in file
-L = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-print("Długość listy (maksymalna liczba ID naliczeń) to: "+ str(len(L)) + " (Pierwszy indeks to zero)")
+L = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+print("Długość listy (maksymalna liczba ID naliczeń) to: "+ str(len(L)-3) + " (Pierwszy indeks to zero)")
 potwierdzenie = input("Liczba wszystkich działek to:" + str(len(listadzialek)) + " Prawidłowo? [T/n]")
 if potwierdzenie == "T" or potwierdzenie == "t":
 
     for h in range(len(listadzialek)):
-        L = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        L = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         nrdzlist = listadzialek[h]
         nrdz = nrdzlist[0]
         print("Obsługuję działkę nr: " + nrdz)
@@ -126,15 +129,25 @@ if potwierdzenie == "T" or potwierdzenie == "t":
 
             i = i + 1
 
-            # Payment section
-        # For first one
+            # Payment and email section
+        # Finding ID for owners
         cur.execute("SELECT IDSIKONTRWLA FROM \"@PZD_RELDZIALKISIKONTR\" WHERE IDDZIALKI='%s'" % iddzialki)
         idsikontrwlarawWLAS = cur.fetchall()
-        # For second one
+        # Getting owner's e-mail adress
+        idaktualnegoraw=idsikontrwlarawWLAS[len(idsikontrwlarawWLAS)-1]
+        idaktualnego=idaktualnegoraw[0]
+        cur.execute("SELECT EMAIL FROM SIKONTR WHERE IDSIKONTR='%s'" % idaktualnego)
+        emailrawraw = cur.fetchall()
+        emailraw=emailrawraw[0]
+        email = emailraw[0]
+        #print("email:")
+        #print(email)
+
+        # Finding ID for co-owners
         cur.execute("SELECT IDSIKONTRMALZ FROM \"@PZD_RELDZIALKISIKONTR\" WHERE IDDZIALKI='%s'" % iddzialki)
         idsikontrwlarawMALZ = cur.fetchall()
 
-        # Adding second one to te first one's list
+        # Joining owners and co-owners ID lists together
         idsikontrwlarawWLAS.extend(idsikontrwlarawMALZ)
         idsikontrwlaraw = idsikontrwlarawWLAS
 
@@ -183,6 +196,8 @@ if potwierdzenie == "T" or potwierdzenie == "t":
         L[41] = kwotanaliczen
         L[42] = KwotaOplat
         L[43] = Saldo
+        if email != None:
+            L[44] = email
 
         f.write("\n")
         for g in range(len(L)):
